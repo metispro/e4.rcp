5f8223655ae5a0eba748e4bbf2daa3ad03aedd93
Tim <timothy.fite@gmail.com>
2013-03-10 17:21:42 -0500

    Adding to e4.rcp repository.

---
+++ e4.rcp.todo.model/src/e4/rcp/todo/model/ITodoModel.java
@@ -0,0 +1,15 @@
+package e4.rcp.todo.model;
+
+import java.util.List;
+
+public interface ITodoModel {
+
+	List<Todo> getTodos();
+	
+	boolean saveTodo(Todo todo);
+	
+	Todo getTodo(long id);
+	
+	boolean deleteTodo(long id);
+	
+}
